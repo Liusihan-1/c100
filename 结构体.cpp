89365274cf69c232005729b6dcd2526af0a6192e
unknown <2028167903@qq.com>
2019-12-01 21:26:04 +0800

    冒泡排序

---
+++ 结构体.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+struct student
+{
+	int ID;
+	char name[20];
+	float score1;
+	float score2;
+	float score3;
+	float avg;
+} 		
+int main()
+{
+    int i = 0;
+    for(i = 0 ; i < N ; i++ )
+    printf("input number: ");
+	scanf("%d", &stu.num );
+	printf("input name: "); 
+	scanf("%s", stu.name);
+	printf("input 3 score:\n");
+	for( i=0;i<3;i++ )
+	{
+		scanf("%f", &stu.score[i]);
+		avg += stu.score[i];
+	}
+	avg /= 3 ;
+	printf("average score: %f\n", avg );
+	return 0;
+}
+
+    
+
+
